d2356c51a6ab1500e14ba045edfc91e657c00675
PREM PALANISAMY <prem.palanisamy.2025@mumail.ie>
2025-04-07 21:26:49 +0100

    git push continued

---
+++ emailService/src/test/java/com/premtsd/linkedin/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premtsd.linkedin.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
